def497ddf2b5569c77ecce64f08810a1435db8fa
Alexandra Crofoot <agcrofoot@crimson.ua.edu>
2020-08-30 15:16:53 -0500

    Final Commit

--- Program.cs
+++ Program.cs
@@ -58,16 +58,29 @@ namespace PA1
                     {
                         Console.Clear();
                         List<Posts> BigAlsPosts = PostFile.GetPosts();
-                        int count = 1;
-                        foreach(Posts post in BigAlsPosts)
+                        List<int> postIDs = new List<int>();
+                        if(BigAlsPosts.Count == 0)
                         {
-                            count++;
+                            Console.WriteLine("Enter your post.");
+                            Posts newPost = new Posts(){ID = 1, Text = Console.ReadLine(), Timestamp = DateTime.Now.ToString()};
+                            Console.WriteLine("Post ID " + newPost.ID + " " + newPost.Text + " " + newPost.Timestamp);
+                            PostFile.SavePost(newPost);
                         }
-                        Console.WriteLine("Enter your post.");
-                        Posts newPost = new Posts(){ID = count++, Text = Console.ReadLine(), Timestamp = DateTime.Now.ToString()};
-                        Console.WriteLine("Post ID " + newPost.ID + " " + newPost.Text + " " + newPost.Timestamp);
-                        PostFile.SavePost(newPost);
+                        else
+                        {
+                            foreach(Posts post in BigAlsPosts)
+                            {
+                                postIDs.Add(post.ID);
+                            }
+                            int lastPost = postIDs.Max();
+                            int currentPost = lastPost + 1;
+                            Console.WriteLine("Enter your post.");
+                            Posts newPost = new Posts(){ID = currentPost, Text = Console.ReadLine(), Timestamp = DateTime.Now.ToString()};
+                            Console.WriteLine("Post ID " + newPost.ID + " " + newPost.Text + " " + newPost.Timestamp);
+                            PostFile.SavePost(newPost);
 
+                        }
+                        
                         Console.WriteLine("Press any key to return to the Menu.");
                         Console.ReadKey();
                         Menu();
@@ -77,41 +90,50 @@ namespace PA1
                     {
                         Console.Clear();
                         List<Posts> BigAlsPosts = PostFile.GetPosts();
-                        BigAlsPosts.Sort();
-                        int count = 0;
-                        foreach(Posts post in BigAlsPosts)
+                        List<int> postIDs = new List<int>();
+                        if(BigAlsPosts.Count == 0)
                         {
-                            Console.WriteLine("Post ID " + post.ID + " " + post.Text + " " + post.Timestamp);
-                            count++;
+                            Console.WriteLine("There are no posts to delete.");
                         }
-                        while(true)
+                        else
                         {
-                            Console.WriteLine("Enter the ID of the post you wish to delete.");
-                            try
+                            BigAlsPosts.Sort();
+                            foreach(Posts post in BigAlsPosts)
                             {
-                                int deleteID = int.Parse(Console.ReadLine());
-                                if(deleteID < 1 || deleteID > count)
+                                Console.WriteLine("Post ID " + post.ID + " " + post.Text + " " + post.Timestamp);
+                                postIDs.Add(post.ID);
+                            }
+                            while(true)
+                            {
+                                Console.WriteLine("Enter the ID of the post you wish to delete.");
+                                try
                                 {
-                                    throw new Exception("Please enter a valid ID.");
-                                }
-                                else
-                                {
-                                    BigAlsPosts.Remove(new Posts(){ID = deleteID, Text = " ", Timestamp = " "});
-                                    foreach(Posts post in BigAlsPosts)
+                                    int deleteID = int.Parse(Console.ReadLine());
+                                    int lastPost = postIDs.Max();
+                                    int firstPost = postIDs.Min();
+                                    if(deleteID < firstPost || deleteID > lastPost)
                                     {
-                                        Console.WriteLine("Post ID " + post.ID + " " + post.Text + " " + post.Timestamp);
+                                        throw new Exception("Please enter a valid ID.");
                                     }
-                                    PostFile.Save(BigAlsPosts);
-                                    Console.WriteLine("Press any key to return to the Menu.");
-                                    Console.ReadKey();
-                                    break;
+                                    else
+                                    {
+                                        BigAlsPosts.Remove(new Posts(){ID = deleteID, Text = " ", Timestamp = " "});
+                                        foreach(Posts post in BigAlsPosts)
+                                        {
+                                            Console.WriteLine("Post ID " + post.ID + " " + post.Text + " " + post.Timestamp);
+                                        }
+                                        PostFile.Save(BigAlsPosts);
+                                        Console.WriteLine("Press any key to return to the Menu.");
+                                        Console.ReadKey();
+                                        break;
+                                    }
+                                }
+                                catch(Exception e)
+                                {
+                                    Console.WriteLine(e.Message);
+                                    continue;
                                 }
                             }
-                            catch(Exception e)
-                            {
-                                Console.WriteLine(e.Message);
-                                continue;
-                            }   
                         }
                     }
                     else if(menuChoice == 4)
